1710845c52d3117772ce23bcf62ae18a2d1c421e
RonaldineP <ronaldinephanor@gmail.com>
2023-04-11 23:09:41 +0000

    simple-counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from "react";
-
+import React,{useState, useEffect} from "react";
+import Digit from "./digit";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Timer } from "./Timer";
 
 //create your first component
-const Home = (props) => {
-	return (
-		<div className="d-flex justify-content-center">
-			<Timer />
-		</div>
-	);
+const Home = () => {
+    let [counter, setCounter] = useState(0)
+    useEffect(()=>{ 
+        setInterval(()=>{
+            setCounter(counter++)
+            console.log(counter)
+        },1000)
+    },[])
+    return (
+        <div className="text-center d-flex count " styles={{alignItems:"center"}}>
+            <Digit count = { Math.floor(counter/100000)}/>
+            <Digit count = { Math.floor(counter/10000)}/>
+            <Digit count = { Math.floor(counter/1000)}/>
+            <Digit count = { Math.floor(counter/100)}/>
+            <Digit count = { Math.floor(counter/10)}/>
+            <Digit count = { Math.floor(counter/1)}/>
+        </div>
+    );
 };
+export default Home;                       
 
-export default Home;
